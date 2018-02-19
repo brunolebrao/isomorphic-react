820df91500d39ee90e7eac58be2ec70a43173692
bruno.lebrao <bruno.lebrao@socialsquad.co>
2018-02-19 12:53:35 -0300

    Server index

--- server/index.jsx
+++ server/index.jsx
@@ -1,13 +1,24 @@
 import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
+import webpack from 'webpack';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get(['/'], function * (req, res){
+if (process.env.NODE_ENV === 'development') {
+    const config = require('../webpack.config.dev.babel').default;
+    const compiler = webpack(config);
+
+    app.use(require('webpack-dev-middleware')(compiler, {noInfo: true}));
+
+    app.use(require('webpack-dev-middleware')(compiler));
+
+}
+
+app.get(['/'], function* (req, res) {
     let index = yield fs.readFile('./public/index.html', "utf-8");
     res.send(index);
 });
 
-app.listen(port, '0.0.0.0', ()=> console.info(`App listening on ${port}`));
+app.listen(port, '0.0.0.0', () => console.info(`App listening on ${port}`));
